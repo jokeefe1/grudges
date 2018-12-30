3c25ea710898acfa7d4ed4ceeb08becdcd39c2b0
John O'Keefe <jokeefe@weegolobal.com>
2018-12-30 03:37:23 -0500

    initial AWS Amplify setup with hosting and auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 import './index.css';
 import Application from './Application';
+
+Amplify.configure(aws_exports);
 
 ReactDOM.render(<Application />, document.getElementById('root'));
